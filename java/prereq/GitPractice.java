e6fae027b89fd5f2c721e33c866aa31eeffb9bc6
DatNhon <s4076839@rmit.edu.vn>
2024-08-30 08:47:58 +0700

    change the in group member 1 again

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -18,7 +18,7 @@ public class GitPractice {
 
    public static void groupMember1() {
       // TODO: Group Member 1, place your code here
-      System.out.println("I'm not the first person in this");
+      System.out.println("I'm Nhon, first member");
    }
 
    public static void groupMember2() {
